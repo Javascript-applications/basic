1c250b4059aa20665331f8fddb92a58f46b06c85
deepakkurmi5 <deepakkurmi874@gmail.com>
2023-08-21 12:33:56 +0530

    completed functions

---
+++ events/event_basics.js
@@ -0,0 +1,36 @@
+// document.getElementById("natureRoot").addEventListener(
+//   "click",
+//   (e) => {
+//     console.log("Clicked inside of natureRoot");
+//   },
+//   false
+// );
+
+// document.getElementById("nature1").addEventListener(
+//   "click",
+//   (e) => {
+//     e.stopPropagation();
+//     console.log("Clicked in nature1");
+//   },
+//   false
+// );
+
+document.getElementById("google").addEventListener(
+  "click",
+  function (e) {
+    console.log("google link clicked");
+    e.preventDefault();
+    e.stopPropagation();
+  },
+  false
+);
+
+document.querySelector("#natureRoot").addEventListener("click", function (e) {
+  let tag = e.target.tagName;
+  if (tag == "IMG") {
+    let removeIt = e.target.parentNode;
+    removeIt.remove();
+    // removeIt.parentNode.removeChild(removeIt);
+    console.log("removeIt", removeIt);
+  }
+});
---
+++ operators/index.js
@@ -0,0 +1,77 @@
+// ==================================================
+// OPERATORS -
+// ==================================================
+
+(1).ArithmeticOpetator;
+
+let a = 3;
+let x = (2 + 2) * a;
+console.log("x", x);
+
+let y = 100 + 10;
+let z = 10;
+z--;
+console.log("z", z);
+
+let m = (100 - 20) * 10;
+
+(2).AssignmentOpetator;
+
+let p = -100;
+p = p >> 5;
+console.log("p", p);
+
+(3).BitwiseOpetator;
+
+let i = 5;
+i >>>= 2;
+console.log("i", i);
+
+(4).LogicalOpetator;
+
+let g = null;
+g &&= 5; // g = g && 5;
+console.log("g", g);
+
+let d;
+d = 4 >= 4 && 2 === 2;
+console.log("&& operator", d);
+
+let r;
+r = 4 != 4 || 2 != 1;
+console.log("|| operator", r);
+
+let v;
+v = 4 != "4";
+console.log("! operator", v);
+
+let t, e;
+t = null;
+n = 11;
+e = 11;
+t = t && t === n;
+console.log("t operator", t);
+
+(5).ComparisionOpetator;
+
+//Example One
+let c = 6;
+if (c <= 8) console.log("Too young to buy alcohol");
+
+const ter = "2" > "12" ? "okay" : " false";
+console.log("ter", ter);
+console.log("12" < "2");
+
+(6).TypeOpetator;
+
+let l = "400";
+let o = +l;
+console.log("Unary + Operator", o, o.toString(), String(4));
+
+let NumToStr = 123.456;
+console.log(
+  "NumToStr",
+  NumToStr.toPrecision(5),
+  NumToStr.toFixed(2),
+  "5" * "2"
+);
