00ee4c6a5d9a963d460350dd951de6475a1d46ad
deepakkurmi5 <deepakkurmi874@gmail.com>
2023-09-28 11:47:26 +0530

    completed events

--- events/event_basics.js
+++ events/event_basics.js
@@ -15,22 +15,26 @@
 //   false
 // );
 
-document.getElementById("google").addEventListener(
-  "click",
-  function (e) {
-    console.log("google link clicked");
-    e.preventDefault();
-    e.stopPropagation();
-  },
-  false
-);
+// document.getElementById("google").addEventListener(
+//   "click",
+//   function (e) {
+//     console.log("google link clicked");
+//     e.preventDefault();
+//     e.stopPropagation();
+//   },
+//   false
+// );
 
-document.querySelector("#natureRoot").addEventListener("click", function (e) {
-  let tag = e.target.tagName;
-  if (tag == "IMG") {
-    let removeIt = e.target.parentNode;
-    removeIt.remove();
-    // removeIt.parentNode.removeChild(removeIt);
-    console.log("removeIt", removeIt);
-  }
-});
+// document.querySelector("#natureRoot").addEventListener(
+//   "click",
+//   function (e) {
+//     let tag = e.target.tagName;
+//     if (tag == "IMG") {
+//       let removeIt = e.target.parentNode;
+//       removeIt.remove();
+//       // removeIt.parentNode.removeChild(removeIt);
+//       console.log("removeIt", removeIt);
+//     }
+//   },
+//   false
+// );
