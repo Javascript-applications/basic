19266fac7e38c217ff052f921369a1565ca0d51b
deepakkurmi5 <deepakkurmi874@gmail.com>
2023-10-21 17:24:48 +0530

    completed object

--- object/property.js
+++ object/property.js
@@ -1,86 +1,86 @@
-const person = {
-  name: "John",
-  age: 30,
-  city: "New York",
-};
+// const person = {
+//   name: "John",
+//   age: 30,
+//   city: "New York",
+// };
 
-console.log(JSON.stringify(person));
+// console.log(JSON.stringify(person));
 
-const arr = Object.values(person);
+// const arr = Object.values(person);
 
-arr.map((item) => {
-  console.log(item);
-});
+// arr.map((item) => {
+//   console.log(item);
+// });
 
-const person = {
-  firstName: "John",
-  lastName: "Doe",
-  language: "en",
-  get lang() {
-    return this.language;
-  },
-};
+// const person = {
+//   firstName: "John",
+//   lastName: "Doe",
+//   language: "en",
+//   get lang() {
+//     return this.language;
+//   },
+// };
 
-console.log(person.lang);
+// console.log(person.lang);
 
-const person = {
-  firstName: "John",
-  lastName: "Doe",
-  language: "",
-  set lang(lang) {
-    this.language = lang;
-  },
-};
+// const person = {
+//   firstName: "John",
+//   lastName: "Doe",
+//   language: "",
+//   set lang(lang) {
+//     this.language = lang;
+//   },
+// };
 
-person.lang = "hindi";
+// person.lang = "hindi";
 
-console.log(person.language);
+// console.log(person.language);
 
-const obj = { count: 0 };
+// const obj = { count: 0 };
 
-Object.defineProperty(obj, "reset", {
-  get: function () {
-    return (this.count = 0);
-  },
-});
+// Object.defineProperty(obj, "reset", {
+//   get: function () {
+//     return (this.count = 0);
+//   },
+// });
 
-Object.defineProperty(obj, "increment", {
-  get: function () {
-    return this.count++;
-  },
-});
+// Object.defineProperty(obj, "increment", {
+//   get: function () {
+//     return this.count++;
+//   },
+// });
 
-Object.defineProperty(obj, "decrement", {
-  get: function () {
-    return this.count--;
-  },
-});
+// Object.defineProperty(obj, "decrement", {
+//   get: function () {
+//     return this.count--;
+//   },
+// });
 
-Object.defineProperty(obj, "add", {
-  set: function (value) {
-    return (this.count += value);
-  },
-});
+// Object.defineProperty(obj, "add", {
+//   set: function (value) {
+//     return (this.count += value);
+//   },
+// });
 
-Object.defineProperty(obj, "subtract", {
-  set: function (value) {
-    return (this.count -= value);
-  },
-});
+// Object.defineProperty(obj, "subtract", {
+//   set: function (value) {
+//     return (this.count -= value);
+//   },
+// });
 
-obj.reset;
-obj.increment;
-obj.decrement;
-obj.add = 5;
-obj.subtract = 2;
+// obj.reset;
+// obj.increment;
+// obj.decrement;
+// obj.add = 5;
+// obj.subtract = 2;
 
-console.log(obj);
+// console.log(obj);
 
-const person = {
-  firstname: "deepak kurmi",
-  greet: function () {
-    return this.firstname;
-  },
-};
+// const person = {
+//   firstname: "deepak kurmi",
+//   greet: function () {
+//     return this.firstname;
+//   },
+// };
 
-console.log(person.greet());
+// console.log(person.greet());
