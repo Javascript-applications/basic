88b55dde973f49e6b578a5123008f853adb423af
deepakkurmi5 <deepakkurmi874@gmail.com>
2023-10-31 10:36:06 +0530

    completed string

---
+++ string/index.js
@@ -0,0 +1,82 @@
+// ===================================
+// String Concept
+// ===================================
+
+// const str = "A string is primitive";
+
+// console.log(str[0]);
+// console.log(str.length);
+
+// const str1 = new Object("A string is primitive");
+
+// console.log("str1", str1);
+
+// const str = "It's somethings 'that' i want \v to tell you";
+
+// console.log(str);
+
+// const str1 = "A string is primitive";
+// const str3 = "A string is primitive";
+// const str2 = new Object("A string is primitive");
+
+// console.log(str1 === str2);
+// console.log(str1 === str3);
+
+// const str1 = "A string is primitive";
+
+// const itretator = str1[Symbol.iterator]();
+
+// for (const iterator of itretator) {
+//   console.log(iterator);
+// }
+
+// const str = "A string is primitive";
+
+// let newStr;
+
+// for (let index = 0; index < str.length; index++) {
+//   const element = str[str.length - index];
+//   newStr += element;
+// }
+
+// console.log(newStr);
+
+// const str = "my name is deepak kurmi";
+
+// console.log(str.length);
+
+// console.log(str.endsWith("deepak", 17));
+
+// const str = "my name is deepak kurmi";
+
+// console.log(str.includes("is"));
+
+// console.log(str.indexOf("is"));
+
+// const str = "my name is deepak kurmi";
+//
+// const paragraph = "The quick brown fox jumps over the lazy dog. It barked.";
+// const regex = /[A-Z]/g;
+// const found = paragraph.match(regex);
+
+// console.log(found);
+
+// const str = "test1test2";
+
+// const regexp = /t(e)(st(\d?))/g;
+
+// const arr = str.matchAll(regexp);
+
+// for (const iterator of arr) {
+//   console.log(iterator);
+// }
+
+// const iterator = str[Symbol.iterator]();
+
+// console.log(iterator.next());
+
+// const srt = "This is deepak kurmi";
+
+// const regex = /[A-Z]/g;
+
+// console.log(srt.search(regex));
---
+++ string/method.js
@@ -0,0 +1,13 @@
+const str = "This is deepak kurmi";
+
+console.log(str.slice(5, -2));
+
+console.log(str.split("a"));
+
+console.log(str.toUpperCase());
+
+const newStr = new String("This is deepak kurmi");
+
+console.log(newStr.toString());
+
+console.log(newStr.valueOf());
